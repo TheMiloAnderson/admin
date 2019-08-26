df794a81fd2c104eaed98eb06f3ea3e38272a0c0
TheMiloAnderson <themiloanderson@gmail.com>
2019-08-25 23:48:25 -0700

    HTTP to HTTPS redirect when site is not at localhost

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 import configureStore from './state/store';
 // Containers
 import DefaultLayout from './containers/DefaultLayout';
+import HttpsRedirect from 'react-https-redirect';
 
 // Bootstrap the store
 var Store = configureStore();
@@ -11,13 +12,15 @@ var Store = configureStore();
 class App extends Component {
   render() {
     return (
-      <Provider store={Store}>
-        <HashRouter>
-          <Switch>
-            <Route path="/" name="Home" component={DefaultLayout} />
-          </Switch>
-        </HashRouter>
-      </Provider>
+      <HttpsRedirect>
+        <Provider store={Store}>
+          <HashRouter>
+            <Switch>
+              <Route path="/" name="Home" component={DefaultLayout} />
+            </Switch>
+          </HashRouter>
+        </Provider>
+      </HttpsRedirect>
     );
   }
 }
